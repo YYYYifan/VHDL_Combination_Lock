4aa9c9e1f94743e03bf798b0aea9a986efe320c7
Mr.Mario <Mr.Mario@users.noreply.github.com>
2020-04-28 18:29:01 +0100

    Update Get_Keys.vhd

--- source_code/Get_Keys.vhd
+++ source_code/Get_Keys.vhd
@@ -30,16 +30,16 @@ begin
         elsif rising_edge(CLK100Hz) then    
             case SWITCHES is 
                 when "0000000000" => null;
-                when "0000000001" => inputValue <= B"0000"; -- 0
-                when "0000000010" => inputValue <= B"0001"; -- 1
-                when "0000000100" => inputValue <= B"0010"; -- 2
-                when "0000001000" => inputValue <= B"0011"; -- 3
-                when "0000010000" => inputValue <= B"0100"; -- 4
-                when "0000100000" => inputValue <= B"0101"; -- 5
-                when "0001000000" => inputValue <= B"0110"; -- 6
-                when "0010000000" => inputValue <= B"0111"; -- 7
-                when "0100000000" => inputValue <= B"1000"; -- 8
-                when "1000000000" => inputValue <= B"1001"; -- 9
+                when "0000000001" => inputValue <= B"0000"; -- SWITCHES[0], number 0
+                when "0000000010" => inputValue <= B"0001"; -- SWITCHES[1], number 1
+                when "0000000100" => inputValue <= B"0010"; -- SWITCHES[2], number 2
+                when "0000001000" => inputValue <= B"0011"; -- SWITCHES[3], number 3
+                when "0000010000" => inputValue <= B"0100"; -- SWITCHES[4], number 4
+                when "0000100000" => inputValue <= B"0101"; -- SWITCHES[5], number 5
+                when "0001000000" => inputValue <= B"0110"; -- SWITCHES[6]，number 6
+                when "0010000000" => inputValue <= B"0111"; -- SWITCHES[7], number 7
+                when "0100000000" => inputValue <= B"1000"; -- SWITCHES[8], number 8
+                when "1000000000" => inputValue <= B"1001"; -- SWITCHES[9], number 9
                 when others         => null;                                           
             end case; 
         end if;
